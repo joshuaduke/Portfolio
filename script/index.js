53e38d2475be76267b0f66ac3808a65b56b5ba44
Joshua Duke <dukejoshuae@gmail.com>
2021-11-02 14:40:13 -0400

    Added layout for project cards

--- script/index.js
+++ script/index.js
@@ -1,6 +1,11 @@
 const hamburger = document.querySelector('.hamburger');
+const closeIcon = document.querySelector('.fa-times');
+const hambIcon = document.querySelector('.fa-bars');
 const nav = document.querySelector('.mobileNav');
+
 
 hamburger.addEventListener('click', ()=>{
     nav.classList.toggle('show');
+    hambIcon.classList.toggle('show');
+    closeIcon.classList.toggle('show');
 })
